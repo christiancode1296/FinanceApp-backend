cc726af4afe29f5fe55179f5d875cd75b5c64e4f
Christian Püschel <christian.code1296@gmail.com>
2025-10-30 17:54:09 +0100

    CORSConfig and Connection allowed to Frontend Port

--- src/main/java/FinanceApp/FinanceApp/FinanceAppApplication.java
+++ src/main/java/FinanceApp/FinanceApp/FinanceAppApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinanceAppApplication {
 
     public static void main(String[] args) {
-        System.out.println("DB_URL = " + System.getenv("DB_URL"));
         SpringApplication.run(FinanceAppApplication.class, args);
     }
 
