bd9feeff8f8ad02b21aefb24830bd22dd89eaaea
Christian Püschel <christian.code1296@gmail.com>
2026-01-02 13:42:08 +0100

    Test

--- src/main/java/FinanceApp/FinanceApp/FinanceAppApplication.java
+++ src/main/java/FinanceApp/FinanceApp/FinanceAppApplication.java
@@ -3,6 +3,7 @@ package FinanceApp.FinanceApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Hauptklasse der Spring Boot-Anwendung
 @SpringBootApplication
 public class FinanceAppApplication {
 
