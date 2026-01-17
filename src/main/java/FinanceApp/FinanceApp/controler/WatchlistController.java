4774ce42249bf32f4646f158a88d2d7fe5e5d623
Christian Püschel <christian.code1296@gmail.com>
2026-01-17 23:01:55 +0100

    Fix transaction management issue in WatchlistController for delete operation

--- src/main/java/FinanceApp/FinanceApp/controler/WatchlistController.java
+++ src/main/java/FinanceApp/FinanceApp/controler/WatchlistController.java
@@ -4,6 +4,7 @@ import FinanceApp.FinanceApp.entity.WatchlistItem;
 import FinanceApp.FinanceApp.repository.WatchlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class WatchlistController {
     }
 
     @DeleteMapping("/{userId}/{symbol}")
+    @Transactional
     public void removeFromWatchlist(@PathVariable String userId, @PathVariable String symbol) {
         repository.deleteByUserIdAndSymbol(userId, symbol);
     }
